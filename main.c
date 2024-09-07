795891471758ec8789f7db186c8bccffbcc083fc
.dawnless <52249849+dawnl3ss@users.noreply.github.com>
2024-09-07 12:19:53 +0200

    update [0.0.2] : started bash

--- main.c
+++ main.c
@@ -5,10 +5,13 @@
 #include "src/specs/specs.h"
 
 int main(){
+    signal(SIGINT, SIG_IGN);
     char hostname[256];
     char username[256];
 
     system("clear");
+
+    int statement = 1;
 
     while (1){
         char command[5000];
@@ -19,8 +22,12 @@ int main(){
         printf("[%s@%s] ~ ", username, hostname);
         fgets(command, 5000, stdin);
 
+        if (command == "exit") statement = 0;
+
+        printf("\n");
         system(command);
         printf("\n");
+
     }
 
     return 0;
